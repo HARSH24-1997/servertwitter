84952875099ef802eaa65a6a5ef6dc370bd589df
HARSH24-1997 <rajput24.harsh@gmail.com>
2021-01-18 22:34:26 +0530

    second

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Twit = require('twit')
+var socketio = require('socket.io')
+var http = require("http");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// var user = require('./models/user');
-// var tweets = require('./models/tweets');
+
+const server = require('http').createServer(app);
+const io =socketio(server);
 
 var app = express();
 const cors = require("cors")
@@ -17,7 +21,7 @@ const session = require("express-session")
 var passport = require("passport")
 const TwitterStrategy = require("passport-twitter").Strategy
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -34,18 +38,11 @@ app.use(cors(
   }
 ))
 
-
 app.use(
   session({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: {
-      maxAge: 100*100*100,
-      path: "/",
-      httpOnly: true
-    },
-    rolling: true
   })
 );
 
@@ -58,7 +55,6 @@ passport.use(new TwitterStrategy({
   callbackURL: "https://twitterserverharsh.herokuapp.com/login/callback"
 },
   function (token, tokenSecret, profile, cb) {
-    //console.log(profile._json)
     const { id, screen_name, profile_image_url, name } = profile._json
     return cb(null, { token, tokenSecret, id, screen_name, profile_image_url, name })
   }
@@ -69,121 +65,78 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((user, done) => {
-  //console.log(user)
   done(null, user);
 });
+
+function twitToken(token, tokenSecret) {
+  return new Twit({
+    consumer_key: process.env.consumerKey,
+    consumer_secret: process.env.consumerSecret,
+    access_token: token,
+    access_token_secret: tokenSecret,
+  })
+}
+
 
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.get('/login',
-  passport.authenticate('twitter'));
-
+app.get('/login', passport.authenticate('twitter'));
 
 app.get("/login/callback", passport.authenticate('twitter'), async (req, res) => {
 
-    const { token, tokenSecret, id, screen_name, profile_image_url, name } = req.session.passport.user
-    //console.log(id)
-//     let T = getT(token, tokenSecret)
-  
-//     var stream2 = T.stream("statuses/filter", { track: screen_name, follow: [id] })
-  
-//     stream2.on('tweet', function (data) {
-  
-//       if (data.user.screen_name !== screen_name) {
-//         //console.log("tweet", data.user.screen_name, screen_name)
-//         io.emit("mention", data)
-//       }
-//       else {
-//         //console.log("follow")
-//         io.emit("follow", data)
-//       }
-  
-//     })
-  
-    res.redirect(`https://helpdesktwitterharsh.netlify.app/logincomplete?&${token}&${tokenSecret}&${screen_name}&${profile_image_url}&${name}`)
-  
+  const { token, tokenSecret, id, screen_name, profile_image_url, name } = req.session.passport.user
+  let Temp = twitToken(token, tokenSecret)
+  var stream2 = Temp.stream("statuses/filter", { track: screen_name, follow: [id] })
+  stream2.on('tweet', function (data) {
+    if (data.user.screen_name !== screen_name) {
+      io.emit("mention", data)
+    }
+    else {
+      io.emit("follow", data)
+    }
   })
-  
+  res.redirect(`https://helpdesktwitterharsh.netlify.app/logincomplete?&${token}&${tokenSecret}&${screen_name}&${profile_image_url}&${name}`)
+})
+
+app.get("/logout", (req, res) => {
+  req.session.passport = null
+  res.send("logout")
+})
+
+
+app.get("/mentions", async (req, res) => {
+  try {
+    const { token, tokenSecret, id, screen_name } = req.session.passport.user
+    let Temp = twitToken(token, tokenSecret)
+    const mentions = await Temp.get(`statuses/mentions_timeline`)
+    const userStatuses = await Temp.get(`statuses/user_timeline`)
+    console.log(mentions.data)
+    console.log(userStatuses.data)
+    var arr =[];
+    res.send(arr.concat(userStatuses.data,mentions.data))
+  }
+  catch (err) {
+    res.sendStatus(400)
+  }
+})
+
+app.post("/reply", async (req, res) => {
+  const { token, tokenSecret, id, screen_name } = req.session.passport.user
+  const { status, in_reply_to_status_id } = req.body
+  let Temp = twitToken(token, tokenSecret)
+  const reply = await Temp.post("/statuses/update", { status, in_reply_to_status_id, auto_populate_reply_metadata: true })
+  res.send("done")
+})
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
-
-
-
-// var Twit = require('twit')
-
-// function getT(token, tokenSecret) {
-//   return new Twit({
-//     consumer_key: process.env.consumerKey,
-//     consumer_secret: process.env.consumerSecret,
-//     access_token: token,
-//     access_token_secret: tokenSecret,
-
-//   })
-
-// }
-
-
-// app.get("/mentions", async (req, res) => {
-//   //console.log("mentions")
-//   try {
-
-//     const { token, tokenSecret, id, screen_name } = req.session.passport.user
-//     let T = getT(token, tokenSecret)
-//     //console.log(screen_name)
-//     const mentions = await T.get(`statuses/mentions_timeline`)
-//     const userStatuses = await T.get(`statuses/user_timeline`)
-//     //console.log(mentions.data, "mentions")
-//     //console.log("status", userStatuses.data)
-//     res.send([...mentions.data, ...userStatuses.data])
-//   }
-//   catch (err) {
-//     res.sendStatus(400)
-//   }
-// })
-
-// app.post("/reply", async (req, res) => {
-//   // //console.log(reply)
-//   const { token, tokenSecret, id, screen_name } = req.session.passport.user
-//   const { status, in_reply_to_status_id } = req.body
-//   let T = getT(token, tokenSecret)
-
-//   const reply = await T.post("/statuses/update", { status, in_reply_to_status_id, auto_populate_reply_metadata: true })
-
-//   res.send("done")
-
-// })
-
-
-
-// app.get("/replies/:id", async (req, res) => {
-//   //console.log("lookup")
-//   const { token, tokenSecret, id, screen_name } = req.session.passport.user
-//   let T = getT(token, tokenSecret)
-//   const tweetId = req.params.id
-//   const lookup = await T.get(`/search/tweets.json?q=@${id}`)
-//   console.table(lookup.data)
-//   //console.log(lookup.data)
-//   // res.send(lookup.data)
-
-// })
-
-// app.get("/logout", (req, res) => {
-//   //console.log("logout")
-//   req.session.passport = null
-//   res.send("logout")
-// })
-
-
-
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
